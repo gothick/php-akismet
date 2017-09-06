345814a076c146625fe5ecde83bdfb291b560f8e
Matt Gibson <gothick@gothick.org.uk>
2017-09-06 21:57:28 +0100

    Reformat in line with coding standards.

--- src/Exception.php
+++ src/Exception.php
@@ -1,10 +1,12 @@
-<?php 
+<?php
 namespace Gothick\AkismetClient;
 
 class Exception extends \Exception
 {
-    public function __construct($message, $code = 0, \Exception $previous = null)
-    {
-        parent::__construct('Gothick\AkismetClient: ' . $message, $code, $previous);
-    }
+
+	public function __construct ($message, $code = 0, \Exception $previous = null)
+	{
+		parent::__construct('Gothick\AkismetClient: ' . $message, $code,
+				$previous);
+	}
 }
