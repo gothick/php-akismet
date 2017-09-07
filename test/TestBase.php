4f8f67a14371e236d8ffc1918bbb0a668e2a9f95
Matt Gibson <gothick@gothick.org.uk>
2017-09-07 15:31:11 +0100

    Test spam/ham responses.

--- test/TestBase.php
+++ test/TestBase.php
@@ -55,6 +55,45 @@ abstract class TestBase extends TestCase
 				'invalid'
 				);
 	}
+
+	protected static function commentCheckHamRepsonse()
+	{
+		return new Response(
+				200,
+				[
+						'Connection' => 'keep-alive',
+						'Content-Length' => '5',
+						'Content-Type' => 'text/plain; charset=utf-8',
+						'Date' => 'Wed, 06 Sep 2017 11:54:28 GMT',
+						'Server' => 'nginx',
+						'X-akismet-alert-code' => '10007',
+						'X-akismet-alert-msg' => "Howdy! We're glad that you're enjoying Akismet! It looks like your Akismet Plus subscription is currently being used on more sites than it supports. You will simply need to access your Akismet account and update your subscription. Please click on the link below for some further details.",
+						// TODO: What *is* the X-akismet-guid? Can we do anything with it?
+						'X-akismet-guid' => '8b3d95a751ebad069dfcee9a3057fc40'
+				],
+				'false'
+		);
+	}
+
+	protected static function commentCheckSpamResponse()
+	{
+		return new Response(
+				200,
+				[
+						'Connection' => 'keep-alive',
+						'Content-Length' => '4',
+						'Content-Type' => 'text/plain; charset=utf-8',
+						'Date' => 'Wed, 06 Sep 2017 11:54:28 GMT',
+						'Server' => 'nginx',
+						'X-akismet-alert-code' => '10007',
+						'X-akismet-alert-msg' => "Howdy! We're glad that you're enjoying Akismet! It looks like your Akismet Plus subscription is currently being used on more sites than it supports. You will simply need to access your Akismet account and update your subscription. Please click on the link below for some further details.",
+						// TODO: What *is* the X-akismet-guid? Can we do anything with it?
+						'X-akismet-guid' => '8b3d95a751ebad069dfcee9a3057fc40'
+				],
+				'true'
+		);
+	}
+
 	protected static function unexpected200Response()
 	{
 		return new Response(
